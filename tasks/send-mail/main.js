d1f40ebc9edc4e724a600fb4994d682e2fd32771
张黎 <az@zhanglideMac-mini.local>
2025-01-09 18:05:59 +0800

    feat: support attachments

--- tasks/send-mail/main.js
+++ tasks/send-mail/main.js
@@ -1,11 +1,20 @@
 export default async function(params, context) {
+  const filePaths = params.attachments;
   const mailer = params.mailer;
   try {
+    let attachments;
+    if (filePaths) {
+      attachments = filePaths.map(filePath => ({
+        fileName: filePath.split('/').pop,
+        path: filePath
+      }))
+    }
     const info = await mailer.sendMail({
       from: params.from,
       to: params.to,
       subject: params.subject,
-      text: params.text
+      text: params.text,
+      attachments: attachments
     })
     console.log(info);
     return {
